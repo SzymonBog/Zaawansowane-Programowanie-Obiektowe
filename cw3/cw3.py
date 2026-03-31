a9cc492bcf656d817e56e73781bafa884d192d5e
SzymonBog <92025573+SzymonBog@users.noreply.github.com>
2026-03-31 16:20:33 +0200

    Add files via upload

---
+++ cw3/cw3.py
@@ -0,0 +1,7 @@
+class FoldersFiles:  # like in hacknet recreation
+    def __init__(self, directory_names, directory_paths, file_paths, file_names, file_extensions):
+        self.dir_name = directory_names
+        self.dir_path = directory_paths
+        self.file_name = file_names
+        self.file_path = file_paths
+        self.file_ext = file_extensions
