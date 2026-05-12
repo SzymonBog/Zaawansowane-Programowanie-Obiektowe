3a3de696bc3737b0b2ee16bbf5b0202cdc5fb3df
SzymonBog <92025573+SzymonBog@users.noreply.github.com>
2026-05-12 02:32:00 +0200

    Update main.py

--- library_project/main.py
+++ library_project/main.py
@@ -8,18 +8,7 @@ from abc import ABC, abstractmethod
 import sqlite3
 
 
-class Singleton:
-    _instance: Self = None
-
-    def __new__(cls, *args, **kwargs) -> Self:
-        if cls._instance is None:
-            instance = super().__new__(cls, *args, **kwargs)
-            cls._instance = instance
-
-        return cls._instance
-
-
-class DatabaseConnection:
+class DatabaseConnection:  # singleton
     _instance: Self = None
 
     def __new__(cls, *args, **kwargs) -> Self:
@@ -33,18 +22,43 @@ class DatabaseConnection:
         self.database_name = database_name
         self.mydb = sqlite3.connect(self.database_name)  # commit
         self.cursor = self.mydb.cursor()  # mysql commands
-    """
-    def connect(self):
-        self.mydb = sqlite3.connect(self.database_name)  # commit
-        self.cursor = self.mydb.cursor()  # mysql commands
-    """
+
+        if self.cursor.execute("select name from sqlite_master where type='table'").fetchall() == []:
+            # table creation
+            self.cursor.execute("create table users(username varchar(255) primary key, password varchar(255), "
+                                "name varchar(20), surname varchar(50), role varchar(255), logged_in int)")
+
+            self.cursor.execute("create table books(title varchar(255), author varchar(255), year int unsigned, "
+                                "copies int unsigned)")
+
+            self.cursor.execute("create table in_possession(username varchar(255), title varchar(255), "
+                                "since timestamp, foreign key(username) references users(username), "
+                                "foreign key(title) references books(title))")
+
+            self.cursor.execute("create table reservations(username varchar(255), title varchar(255), "
+                                "since timestamp, foreign key(username) references users(username), "
+                                "foreign key(title) references books(title))")
+            # the same table as in_possession but works differently
+
+            self.cursor.execute("create table history(username varchar(255), action varchar(20), "
+                                "sql_command varchar(1000), reverse_sql_command varchar(1000))") # action = rent/return
+
+            # save tables/changes to db
+            self.mydb.commit()
+            print(f"Database {self.database_name} created")
+        else:
+            # for testing
+            print(self.cursor.execute("select name from sqlite_master where type='table'").fetchall())
+            print(self.cursor.execute("select name from sqlite_master where type='table'").fetchall()[0])
+            print(self.cursor.execute("select name from sqlite_master where type='table'").fetchall()[0][0])
+            pass
 
 
 class User(ABC):
     pass
 
 
-db = DatabaseConnection("library_database.db")
-db1 = DatabaseConnection("library_database.db")
+mydb = DatabaseConnection("library_database.db")
+# mydb1 = DatabaseConnection("library_database.db")
 
-print(db is db1)
+# print(mydb is mydb1)
